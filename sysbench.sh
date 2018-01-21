f7efc0c0442ac661df6d3d862b105d6ebc592e92
LAPTOP-RKNG9MQB\heyhe <heyheyforhello@outlook.com>
2018-01-21 09:14:25 -0500

    Add sysbench script

---
+++ sysbench.sh
@@ -0,0 +1,3 @@
+sysbench --test=cpu --cpu-max-prime=20000 --num-threads=8 run
+sysbench --test=cpu --cpu-max-prime=40000 --num-threads=1 run
+sysbench --test=cpu --cpu-max-prime=50000 --num-threads=4 run
