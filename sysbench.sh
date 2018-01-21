f069d3e416f2142f78bd74908872ed241a31a982
LAPTOP-RKNG9MQB\heyhe <heyheyforhello@outlook.com>
2018-01-21 09:20:51 -0500

    Add update and install for the sysbench script

--- sysbench.sh
+++ sysbench.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt-get install sysbench
 sysbench --test=cpu --cpu-max-prime=20000 --num-threads=8 run
 sysbench --test=cpu --cpu-max-prime=40000 --num-threads=1 run
 sysbench --test=cpu --cpu-max-prime=50000 --num-threads=4 run
